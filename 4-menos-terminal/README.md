278f4cb9198f5170237d80eb82ca1c5d1631d960
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-02-19 21:37:33 -0300

    Exemplo de mapa interativo

---
+++ 4-menos-terminal/README.md
@@ -0,0 +1,4 @@
+# Mapas nem tão no terminal assim
+
+*`0-obtem-dados.sh`* Baixa dados do IBGE, cruza eles com os dados do QEDU e produz um GeoJSON com as propriedades de cada feature incluindo os dados do QEDU.
+*`1-cria-mapa.sh`* Usa ferramentas de linha de comando para gerar um choropleth.
---
+++ 4-menos-terminal/legenda-d3-cor.js
@@ -0,0 +1,41 @@
+/*
+ * LEGENDA
+ */
+var desenhaLegenda = function(min, max, escalaDeCor, nomeVariavel){
+  var x = d3.scaleLinear()
+      .domain([min, max])
+      .rangeRound([600, 860]);
+
+  var g = svg.append("g")
+      .attr("class", "key")
+      .attr("transform", "translate(0,40)");
+
+  g.selectAll("rect")
+    .data(escalaDeCor.range().map(function(d) {
+        d = escalaDeCor.invertExtent(d);
+        if (d[0] == null) d[0] = x.domain()[0];
+        if (d[1] == null) d[1] = x.domain()[1];
+        return d;
+      }))
+    .enter().append("rect")
+      .attr("height", 8)
+      .attr("x", d => x(d[0]))
+      .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+      .attr("fill", function(d) { return escalaDeCor(d[0]); });
+
+  g.append("text")
+      .attr("class", "caption")
+      .attr("x", x.range()[0])
+      .attr("y", -6)
+      .attr("fill", "#000")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text(nomeVariavel);
+
+  g.call(d3.axisBottom(x)
+      .tickSize(13)
+      .tickFormat(function(x, i) { return (i ? x : x + "%"); })
+      .tickValues(escalaDeCor.domain()))
+    .select(".domain")
+      .remove();
+}
