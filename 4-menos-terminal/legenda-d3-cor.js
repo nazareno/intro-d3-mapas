6edb04bfd53ae8524695e5ea9d3436ca9d27052d
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-02-20 10:43:39 -0300

    Melhora desenho da legenda para lidar com mínimo

--- 4-menos-terminal/legenda-d3-cor.js
+++ 4-menos-terminal/legenda-d3-cor.js
@@ -35,7 +35,7 @@ var desenhaLegenda = function(min, max, escalaDeCor, nomeVariavel){
   g.call(d3.axisBottom(x)
       .tickSize(13)
       .tickFormat(function(x, i) { return (i ? x : x + "%"); })
-      .tickValues(escalaDeCor.domain()))
+      .tickValues(escalaDeCor.domain().concat(min)))
     .select(".domain")
       .remove();
 }
